c5ea817fc3926208d89700c71c2292110a50b064
Charles de Dreuille <charles.dedreuille@gmail.com>
2019-12-02 20:25:51 +0000

    Update UI

---
+++ src/code.js
@@ -0,0 +1,17 @@
+import transformer from './converter/transformer';
+figma.showUI(__html__);
+figma.ui.onmessage = msg => {
+    switch (msg.type) {
+        case 'convert':
+            const figmaPages = figma.root.children;
+            const sketchPages = figmaPages.map(page => transformer(page));
+            figma.ui.postMessage({ figmaPages, sketchPages });
+            break;
+        case 'download':
+            figma.closePlugin();
+            break;
+        // Is this needed?
+        default:
+            break;
+    }
+};
