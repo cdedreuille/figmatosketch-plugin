005c38a99ebebac18582f3b02fa813b06a9285fc
Charles de Dreuille <charles.dedreuille@gmail.com>
2019-12-04 18:21:16 +0000

    Fix

--- src/converter/shapes/vector.js
+++ src/converter/shapes/vector.js
@@ -5,45 +5,83 @@ module.exports = function (data, result) {
 
   console.log(data.vectorNetwork);
 
-  data.vectorNetwork.segments.map(segment => {
-    const vertice = data.vectorNetwork.vertices[segment.end];
+  // data.vectorNetwork.segments.map(segment => {
+  //   const vertice = data.vectorNetwork.vertices[segment.end];
+  //   let boom = {
+  //     "_class": "curvePoint",
+  //     "cornerRadius": 0,
+  //     "point": `{${vertice.x/objWidth}, ${vertice.y/objHeight}}`,
+  //     "curveFrom": `{${segment.tangentStart.x/objWidth}, ${segment.tangentStart.y/objHeight}}`,
+  //     "curveTo": `{${segment.tangentEnd.x/objWidth}, ${segment.tangentEnd.y/objHeight}}`
+  //   }
+  //
+  //   const ts = segment.tangentStart;
+  //   const te = segment.tangentEnd;
+  //
+  //   if (ts.x > 0 || ts.y > 0 || ts.x < 0 || ts.y < 0) {
+  //     if (te.x === 0 && te.y === 0) {
+  //       boom.hasCurveFrom = false;
+  //       boom.hasCurveTo = true;
+  //     } else {
+  //       boom.hasCurveFrom = true;
+  //       boom.hasCurveTo = true;
+  //     }
+  //   } else if (ts.x === 0 && ts.y === 0) {
+  //     if (te.x === 0 && te.y === 0) {
+  //       boom.hasCurveFrom = false;
+  //       boom.hasCurveTo = false;
+  //     } else {
+  //       boom.hasCurveFrom = true;
+  //       boom.hasCurveTo = false;
+  //     }
+  //   }
+  //
+  //   // https://www.figma.com/plugin-docs/api/HandleMirroring/
+  //   // Figma doesn't have curveMode 3. No need for it.
+  //   if (vertice.handleMirroring === "ANGLE") {
+  //     boom.curveMode = 4;
+  //   } else if (vertice.handleMirroring === "ANGLE_AND_LENGTH") {
+  //     boom.curveMode = 2;
+  //   } else {
+  //     boom.curveMode = 1;
+  //   }
+  //
+  //   newPath.push(boom);
+  // });
+
+  data.vectorNetwork.vertices.map((vertice, key) => {
+    const segment = data.vectorNetwork.segments[key];
+    const thing = data.vectorNetwork.segments.find(item => {
+      return item.start === key;
+    });
+
     let boom = {
       "_class": "curvePoint",
       "cornerRadius": 0,
       "point": `{${vertice.x/objWidth}, ${vertice.y/objHeight}}`,
-      "curveFrom": `{${segment.tangentStart.x/objWidth}, ${segment.tangentStart.y/objHeight}}`,
-      "curveTo": `{${segment.tangentEnd.x/objWidth}, ${segment.tangentEnd.y/objHeight}}`
+      "curveTo": `{${thing.tangentStart.x/objWidth}, ${thing.tangentStart.y/objHeight}}`,
+      "curveFrom": `{${thing.tangentEnd.x/objWidth}, ${thing.tangentEnd.y/objHeight}}`
     }
 
-    const ts = segment.tangentStart;
-    const te = segment.tangentEnd;
-
-    if (ts.x > 0 || ts.y > 0 || ts.x < 0 || ts.y < 0) {
-      if (te.x === 0 && te.y === 0) {
-        boom.hasCurveFrom = false;
-        boom.hasCurveTo = true;
-      } else {
-        boom.hasCurveFrom = true;
-        boom.hasCurveTo = true;
-      }
-    } else if (ts.x === 0 && ts.y === 0) {
-      if (te.x === 0 && te.y === 0) {
-        boom.hasCurveFrom = false;
+    if (thing.tangentStart.x === 0 && thing.tangentStart.y === 0) {
+      if (thing.tangentEnd.x === 0 && thing.tangentEnd.y === 0) {
         boom.hasCurveTo = false;
+        boom.hasCurveFrom = false;
       } else {
-        boom.hasCurveFrom = true;
         boom.hasCurveTo = false;
+        boom.hasCurveFrom = true;
       }
-    }
-
-    // https://www.figma.com/plugin-docs/api/HandleMirroring/
-    // Figma doesn't have curveMode 3. No need for it.
-    if (vertice.handleMirroring === "ANGLE") {
-      boom.curveMode = 4;
-    } else if (vertice.handleMirroring === "ANGLE_AND_LENGTH") {
-      boom.curveMode = 2;
+    } else if (thing.tangentEnd.x === 0 && thing.tangentEnd.y === 0) {
+      if (thing.tangentStart.x === 0 && thing.tangentStart.y === 0) {
+        boom.hasCurveTo = false;
+        boom.hasCurveFrom = false;
+      } else {
+        boom.hasCurveTo = true;
+        boom.hasCurveFrom = false;
+      }
     } else {
-      boom.curveMode = 1;
+      boom.hasCurveTo = true;
+      boom.hasCurveFrom = true;
     }
 
     newPath.push(boom);
